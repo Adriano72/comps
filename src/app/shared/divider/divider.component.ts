f198e8492938375f9a8c6f91d2842581f0e37996
Adriano Massi <adrianomassi@gmail.com>
2022-08-31 23:59:01 +0200

    Update

---
+++ src/app/shared/divider/divider.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.css']
+})
+export class DividerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
