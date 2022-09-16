6ad283b623d72ea55e67eb26712e21196064357b
Adriano Massi <a.massi@informatica.aci.it>
2022-09-16 12:27:05 +0200

    Completed Section 10

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ViewsRoutingModule } from './views-routing.module';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ViewsHomeComponent } from './views-home/views-home.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
   declarations: [
     StatisticsComponent,
-    ViewsHomeComponent
+    ViewsHomeComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
