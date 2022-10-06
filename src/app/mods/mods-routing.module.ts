b179cca93bef8535673f0f2178c15431c5e86c63
Adriano Massi <a.massi@informatica.aci.it>
2022-10-06 16:25:14 +0200

    Beginning section 11

--- src/app/mods/mods-routing.module.ts
+++ src/app/mods/mods-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    component: ModsHomeComponent,
+    children: [
+      {path: '', component: ModsHomeComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
